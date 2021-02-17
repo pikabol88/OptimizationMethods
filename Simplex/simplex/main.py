0ab9dc7fc76f77877411b383f38924530c305c9a
derkanw <60828039+derkanw@users.noreply.github.com>
2021-02-17 15:44:46 +0300

    Add brute force method

--- Simplex/simplex/main.py
+++ Simplex/simplex/main.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from simplex.canonical import to_canon
 from simplex.dual_task import to_dual_task
+from simplex.extreme_point import brute_force
 from simplex.print import print_task
 from simplex.parse import parse
 from simplex.simplex import simplex
@@ -25,6 +26,13 @@ def main():
     dual_N, dual_B, dual_A, dual_b, dual_c, dual_v = to_canon(dual_matrix, dual_line_signs, dual_free_vec,
                                                               dual_var_signs, dual_target)
     print_task(dual_matrix, dual_line_signs, dual_free_vec, dual_var_signs, dual_target, message)
+
+    print("***Solution of direct task by brute force method***")
+    print(brute_force(A, b, c))
+    print()
+    print("***Solution of dual task by brute force method***")
+    print(brute_force(dual_A, dual_b, dual_c))
+    print("============================================================================")
 
     x = simplex(N, B, A, b, c, v)
     print(x)
