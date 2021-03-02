fb84ee87eda952099af3c5920c71ffe0610a8cdc
Alyona Voinova <nena.2000@mail.ru>
2021-03-02 19:43:33 +0300

    Create second lab folders

--- TransportTask/transportTask/main.py
+++ TransportTask/transportTask/main.py
@@ -1,16 +1,78 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import tabulate
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class TransportProblem:
+    _demand = []
+    _supply = []
+    _costs = []
+    _matrix = []
+
+    def __init__(self, filename):
+        with open(filename, 'r') as f:
+            problem = f.read().splitlines()
+        supply = problem.pop(0).split(' ')
+        demand = problem.pop(0).split(' ')
+        self._supply = [int(item) for item in supply]
+        self._demand = [int(item) for item in demand]
+        print("Проверим задачу на замкнутость:")
+        sum_sup = sum(self._supply)
+        sum_dem = sum(self._demand)
+        print("Сумма запасов:" + str(sum_sup))
+        print("Сумма спросов:" + str(sum_dem))
+        if sum_sup > sum_dem:
+            print("Приведем задачу к замкнутому виду, добавив фиктовного поставщика")
+            self._demand.append(sum_sup-sum_dem)
+            for i in range(len(self._supply)):
+                self._matrix[i].append(0)
+        elif sum_sup < sum_dem:
+            print("Приведем задачу к замкнутому виду, добавив фиктовного заказчика")
+            self._supply.append(sum_dem-sum_sup)
+            tmp = []
+            for i in range(len(self._demand)):
+                tmp.append(0)
+            self._matrix.append(tmp)
+        else:
+            print("Задача замкнутого вида")
+        while len(problem) > 0:
+            self._matrix.append(problem.pop(0).split(' '))
+
+    def print_table(self):
+        res_i = []
+        rows = []
+        headers = [""]
+        num = 0
+        for item in self._demand:
+            string = "consumer " + str(num) + " need " + str(item)
+            headers.append(string)
+            num += 1
+        num = 0
+        for item in self._supply:
+            string = "supplier " + str(num) + " supply " + str(item)
+            res_i.append(string)
+            for j in range(len(self._demand)):
+                res_i.append(self._matrix[num][j])
+            num += 1
+            rows.append(res_i.copy())
+            res_i.clear()
+        # tablefmt="latex"
+        print(tabulate.tabulate(rows, headers))
 
 
-# Press the green button in the gutter to run the script.
+class Shipment:
+    cosPerUnit = 0
+    r = 0
+    c = 0
+    quantity = 0
+
+    def __init__(self, cpu, q, r, c):
+        self.quantity = q
+        self.cosPerUnit = cpu
+        self.c = c
+        self.r = r
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("\nTRANSPORT PROBLEM:\n")
+    tp = TransportProblem("problem.txt")
+    tp.print_table()
