d67efb0b9e7de493a03ae662a815eea7136c5f55
ThinkingFrog <khripunkovdv@gmail.com>
2021-03-02 23:01:13 +0300

    Refactor problem class

--- TransportTask/transport/tproblem.py
+++ TransportTask/transport/tproblem.py
@@ -2,39 +2,45 @@ import tabulate
 
 
 class TransportProblem:
-    _demand = []
-    _supply = []
-    _costs = []
-    _matrix = []
-
     def __init__(self, filename):
+        self._demand = list()
+        self._supply = list()
+        self._costs = list()
+        self._matrix = list()
+
         with open(filename, 'r') as f:
             problem = f.read().splitlines()
-        supply = problem.pop(0).split(' ')
-        demand = problem.pop(0).split(' ')
+
+        supply = problem.pop(0).split()
         self._supply = [int(item) for item in supply]
+
+        demand = problem.pop(0).split()
         self._demand = [int(item) for item in demand]
+        
         while len(problem) > 0:
             matrix = problem.pop(0).split(' ')
-            self._matrix.append(matrix)
+            self._matrix.append([int(item) for item in matrix])
+        
         print("Проверим задачу на замкнутость:")
+        
         sum_sup = sum(self._supply)
         sum_dem = sum(self._demand)
         print("Сумма запасов:" + str(sum_sup))
         print("Сумма спросов:" + str(sum_dem))
+
         if sum_sup > sum_dem:
             print("Приведем задачу к замкнутому виду, добавив фиктовного поставщика")
-            self._demand.append(sum_sup-sum_dem)
-            for i in range(len(self._supply)):
-                self._matrix[i].append(0)
+            self._demand.append(sum_sup - sum_dem)
+            for line in self._matrix:
+                line.append(0)
+        
         elif sum_sup < sum_dem:
             print("Приведем задачу к замкнутому виду, добавив фиктовного заказчика")
             self._supply.append(sum_dem-sum_sup)
-            tmp = []
-            for i in range(len(self._demand)):
-                tmp.append(0)
-            self._matrix.append(tmp)
-        else:
+            self._matrix.append([0] * len(self._matrix[0]))
+            pass
+        
+        else:    
             print("Задача замкнутого вида")
 
 
