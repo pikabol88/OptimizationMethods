424de68c622ec534858fbb8de86899d5d3d80572
Alyona Voinova <nena.2000@mail.ru>
2021-03-17 02:05:04 +0300

    Add Payell method

--- OneDimMinimization/onedim/parabolic.py
+++ OneDimMinimization/onedim/parabolic.py
@@ -2,6 +2,7 @@ from typing import List
 
 from onedim.function import Function
 
+
 def parabolic(fun: Function, x: float) -> float:
     eps = 0.1
     iter = 0
@@ -10,9 +11,9 @@ def parabolic(fun: Function, x: float) -> float:
 
     x1 = fun.x_min
     x3 = fun.x_max
-    
+
     while abs(q - q_prev) > eps:
-        iter +=1
+        iter += 1
         print(iter)
         q_prev = q
 
@@ -31,3 +32,45 @@ def parabolic(fun: Function, x: float) -> float:
         q = a0 + a1 * (x - x1) + a2 * (x - x1) * (x - x2)
 
     return q
+
+
+class PointValue:
+    def __init__(self, x, f_x) -> None:
+        self.x = x
+        self.y = f_x
+
+
+def payell_method(fun: Function, eps: float):
+    h = 0.1
+    x1 = 1
+    iter_num = 0
+    while 1:
+        iter_num += 1
+        x2 = x1 + h
+        f1 = PointValue(x1, fun.func(x1))
+        f2 = PointValue(x2, fun.func(x2))
+        if f1.y > f2.y:
+            x3 = x1 + 2 * h
+        else:
+            x3 = x1 - h
+        f3 = PointValue(x3, fun.func(x3))
+        if f1.y < f2.y:
+            if f1.y < f3.y:
+                f_min = f1
+            else:
+                f_min = f3
+        elif f2.y < f3.y:
+            f_min = f2
+        else:
+            f_min = f3
+
+        a1 = (f2.y - f1.y) / (f2.x - f1.x)
+        a2 = 1 / (f3.x - f2.x) * (((f3.y - f1.y) / (f3.x - f1.x)) - ((f2.y - f1.y) / (f2.x - f1.x)))
+        x_res = (f2.x - f1.x) / 2 - a1 / 2 * a2
+        if abs(x_res - f_min.x) < eps:
+            return x_res, fun.func(x_res), iter_num
+        else:
+            if fun.func(x_res) < f_min.y:
+                x1 = x_res
+            else:
+                x1 = f_min.x
