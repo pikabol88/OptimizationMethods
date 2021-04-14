d01c87b5f079d0ea21eff5bc4accd0d126f8d7d9
ThinkingFrog <khripunkovdv@gmail.com>
2021-04-14 20:21:18 +0300

    Add commentaries

--- MultiDimWithRestrictions/multidim/zoytendeyk.py
+++ MultiDimWithRestrictions/multidim/zoytendeyk.py
@@ -3,7 +3,14 @@ from multidim.function import Function
 from scipy.optimize import linprog
 
 def zoytendeyk(x0: List[float], eta: int, fi0: Function, fi: List[Function]) -> List[float]:
+    # Начальный этап
     ksi: List[int] = [1 for idx in range(len(fi) + 1)]
     lam = 0.5
+    delta = -eta
+    xk = x0
 
-    eta = linprog(eta).x[0]
+    #  Поиск начального приближения
+    min_eta = linprog(fi).x[0]
+    
+    # Основной этап
+    
