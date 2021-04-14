30e7435d4f026276d8c4865f52908c09549e9663
ThinkingFrog <khripunkovdv@gmail.com>
2021-04-14 15:25:11 +0300

    Zoytendeyk PoC

--- MultiDimWithRestrictions/multidim/zoytendeyk.py
+++ MultiDimWithRestrictions/multidim/zoytendeyk.py
@@ -1,5 +1,9 @@
 from typing import List
 from multidim.function import Function
+from scipy.optimize import linprog
 
-def zoytendeyk(x0: List[float], eta0: int, fi0: Function, fi: List[Function]) -> List[float]:
-    pass
+def zoytendeyk(x0: List[float], eta: int, fi0: Function, fi: List[Function]) -> List[float]:
+    ksi: List[int] = [1 for idx in range(len(fi) + 1)]
+    lam = 0.5
+
+    eta = linprog(eta).x[0]
