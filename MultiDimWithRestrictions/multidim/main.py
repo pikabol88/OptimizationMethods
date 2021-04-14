9d07813344bd2edcfd89cf8a80d4d008affe1093
ThinkingFrog <khripunkovdv@gmail.com>
2021-04-15 01:42:57 +0300

    Update zoytendeyk

--- MultiDimWithRestrictions/multidim/main.py
+++ MultiDimWithRestrictions/multidim/main.py
@@ -1,9 +1,13 @@
 from multidim.restrictions import plot_restrictions
 from multidim.zoytendeyk import zoytendeyk
 
-def main():
-    plot_restrictions(point_on_boarder=False)
-    plot_restrictions(point_on_boarder=True)
+import numpy as np
 
-    res = zoytendeyk([0, 0], 1, )
+def main():
+    
+    # plot_restrictions(point_on_boarder=False)
+    # plot_restrictions(point_on_boarder=True)
+
+    res = zoytendeyk([0.2, 0.4], 0.5)
+    print(res)
     return
--- MultiDimWithRestrictions/multidim/zoytendeyk.py
+++ MultiDimWithRestrictions/multidim/zoytendeyk.py
@@ -1,33 +1,76 @@
-import copy 
-
 from typing import List
-from multidim.function import Function
 from scipy.optimize import linprog
 
-def zoytendeyk(x0: List[float], eta: int, fi0: Function, fi: List[Function]) -> List[float]:
+import numpy as np
+
+
+func = lambda x: 4 * x[0] + x[1] + 4 * np.sqrt(1 + 3 * x[0] ** 2 + x[1] ** 2)
+func_grad = lambda x: [12 * (x[0] / np.sqrt(1 + 3 * x[0] ** 2 + x[1] ** 2)) + 4,
+                       4 * (x[1] / np.sqrt(1 + 3 * x[0] ** 2 + x[1] ** 2)) + 1]
+
+rest = [
+    lambda x: -x[0],
+    lambda x: x[0] - 2 * x[1] - 0.3,
+    lambda x: -x[0] + 2 * x[1] - 1,
+    lambda x: x[0] + x[1] - 1
+]
+
+rest_grads = [
+    lambda x: [-1, 0],
+    lambda x: [1, -2],
+    lambda x: [-1, 2],
+    lambda x: [1, 1]
+]
+
+
+def get_bounds(x, delta):
+    res = []
+    for idx in range(len(rest)):
+        if -delta <= rest[idx](x) <= 0.0:
+            res.append(idx)
+    return res
+
+
+def simplex(x_k, d_k):
+    bounds_idxs = get_bounds(x_k, d_k)
+    aux_matrix = np.zeros(shape=(1 + len(bounds_idxs), 3))
+
+    aux_matrix[:, 2] = -1
+    aux_matrix[0, 0:2] = func_grad(x_k)
+
+    j = 1
+    for i in bounds_idxs:
+        aux_matrix[j, 0:2] = rest_grads[i](x_k)
+        j += 1
+
+    target_coefs = np.zeros(3)
+    target_coefs[2] = 1
+
+    bias = np.zeros(aux_matrix.shape[0])
+    bounds = [[-1, 1], [-1, 1], [None, None]]
+
+    return linprog(c=target_coefs, A_ub=aux_matrix, b_ub=bias, bounds=bounds, method='simplex').x
+
+
+def zoytendeyk(x0: List[float], eta: int) -> List[float]:
     # Начальный этап
-    ksi: List[int] = [1 for idx in range(len(fi) + 1)]
     alpha = 1
     lam = 0.5
     delta = -eta
     x = x0
 
-    #  Поиск начального приближения
-    # min_eta = linprog(fi, method='simplex').x[0]
-    
     # Основной этап
     while True:
-        c = copy.deepcopy(ksi)
-        A_ub = [[fun._F1, fun._F2] for fun in zip([fi0], fi)]
-        b_ub = [eta * ksi_k for ksi_k in ksi]
-        s, eta = linprog(c=c, A_ub=A_ub, b_ub=b_ub, method='simplex').x
+        *s, eta = simplex(x, delta)
 
         if eta < delta:
-            x = [x_k + alpha * s for x_k in x]
+            x = [x_k + alpha * s_k for x_k, s_k in zip(x, s)]
         else:
             delta *= lam
+        
+        print(x)
 
-        if delta < min([fun.f(x[0], x[1]) for fun in zip([fi0], fi)]) and eta == 0:
+        if delta < min([func(x)] + [r(x) for r in rest]) and eta < 1e-6:
             break
     
     return x
