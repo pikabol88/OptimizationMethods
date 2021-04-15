e69c45c7b6c8fb320c93f4985c3af8b28a00c207
Alyona Voinova <nena.2000@mail.ru>
2021-04-15 03:36:30 +0300

    Change min point

--- MultiDimWithRestrictions/multidim/restrictions.py
+++ MultiDimWithRestrictions/multidim/restrictions.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 
 def plot_restrictions(point_on_boarder=False):
     x_point = -0.4288
-    y_point = -0.3213
+    y_point = -0.3216
 
     d = np.linspace(-5, 5, 300)
     x1, x2 = np.meshgrid(d, d)
 
     if point_on_boarder:
         plt.imshow(
-            ((x1 <= 0) & (x2 <= 0) & (-x1 - x2 - 0.7501 <= 0) & (x1 - 2 * x2 - 1 <= 0)).astype(int),
+            ((x1 <= 0) & (x2 <= 0) & (-x1 - x2 - 0.7504 <= 0) & (x1 - 2 * x2 - 1 <= 0)).astype(int),
             extent=(x1.min(), x1.max(), x2.min(), x2.max()), origin="lower", cmap="Greys", alpha=0.3)
     else:
         plt.imshow(((x1 <= 0) & (x2<=0) & (-x1 - x2 - 1 <= 0) & (x1 - 2 * x2 - 1 <= 0)).astype(int),
@@ -22,7 +22,7 @@ def plot_restrictions(point_on_boarder=False):
     y2 =x1*0
     y3 = (x1 - 1) / 2
     if point_on_boarder:
-        y4 = -0.7501 - x1
+        y4 = -0.7504 - x1
     else:
         y4 = -1 - x1
 
@@ -30,7 +30,7 @@ def plot_restrictions(point_on_boarder=False):
     plt.plot(x1, y2, label=r'$x_2\leq0$')
     plt.plot(x1, y3, label=r'$x_1-2x_2-1\leq0$')
     if point_on_boarder:
-        plt.plot(x1, y4, label=r'$-x_1-x_2-0.7501\leq0$')
+        plt.plot(x1, y4, label=r'$-x_1-x_2-0.7504\leq0$')
     else:
         plt.plot(x1, y4, label=r'$-x_1-x_2-1\leq0$')
 
