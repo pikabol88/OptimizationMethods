045e3a1d747ac5f363d8b55ee1f8ffaf34c5bec6
ThinkingFrog <khripunkovdv@gmail.com>
2021-04-25 13:49:01 +0300

    Add eta0 computing

--- MultiDimWithRestrictions/multidim/main.py
+++ MultiDimWithRestrictions/multidim/main.py
@@ -1,12 +1,15 @@
 from multidim.restrictions import plot_restrictions
-from multidim.zoytendeyk import zoytendeyk, func
+from multidim.zoytendeyk import zoytendeyk, func, rest
 
 def main():
     
-    plot_restrictions(point_on_boarder=False)
-    plot_restrictions(point_on_boarder=True)
+    # plot_restrictions(point_on_boarder=False)
+    # plot_restrictions(point_on_boarder=True)
 
-    res = zoytendeyk([-0.2, -0.4], 0.1)
+    x0 = [-0.3, -0.4]
+    eta0 = -max([fun(x0) for fun in rest])
+
+    res = zoytendeyk(x0, eta0)
     print(f"Answer: {res}")
     print(f"f(x) = {func(res)}")
     
