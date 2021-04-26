0ac3a9846fd7dc496aae20685558cb46ae357829
ThinkingFrog <khripunkovdv@gmail.com>
2021-04-26 18:50:06 +0300

    Final ?

--- LAB6/sticks/main.py
+++ LAB6/sticks/main.py
@@ -1,3 +1,4 @@
+import math
 from scipy.optimize import linprog
 
 
@@ -13,4 +14,11 @@ def main():
 
     solution = linprog(c=target_func, A_ub=matrix, b_ub=free_vec, method='simplex').x
 
-    print(len(solution))
+    not_zero = list()
+    sum = 0
+    for idx, num in enumerate(solution):
+        if num != 0:
+            not_zero.append([idx, math.ceil(num)])
+            sum += math.ceil(num)
+    print(not_zero)
+    print(sum)
