7a5ea2b81777de449f56d8d42ad05858e73a4218
ThinkingFrog <khripunkovdv@gmail.com>
2021-05-09 20:32:51 +0300

    Recognition sketch

---
+++ Coursework/recognition/main.py
@@ -0,0 +1,87 @@
+import pylab as pl
+import numpy as np
+
+from matplotlib import pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import fetch_lfw_people
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.decomposition import PCA as RandomizedPCA
+from sklearn.svm import SVC
+
+def title(y_pred, y_test, target_names, i):
+    pred_name = target_names[y_pred[i]].rsplit(' ', 1)[-1]
+    true_name = target_names[y_test[i]].rsplit(' ', 1)[-1]
+    return 'predicted: %s\ntrue:      %s' % (pred_name, true_name)
+
+
+def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
+    """Helper function to plot a gallery of portraits"""
+    plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
+    plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
+    for i in range(n_row * n_col):
+        plt.subplot(n_row, n_col, i + 1)
+        plt.imshow(images[i].reshape((h, w)), cmap=plt.cm.gray)
+        plt.title(titles[i], size=12)
+        plt.xticks(())
+        plt.yticks(())
+
+
+def main():
+    lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
+    
+    n_samples, h, w = lfw_people.images.shape
+    np.random.seed(42)
+    
+    X = lfw_people.data
+    n_features = X.shape[1]
+
+    y = lfw_people.target
+    target_names = lfw_people.target_names
+    n_classes = target_names.shape[0]
+
+    print("Total dataset size:")
+    print(f"n_samples: {n_samples}")
+    print(f"n_features: {n_features}")
+    print(f"n_classes: {n_classes}")
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+
+    n_components = 50
+    pca = RandomizedPCA(n_components=n_components, whiten=True).fit(X_train)
+
+    eigenfaces = pca.components_.reshape((n_components, h, w))
+
+    X_train_pca = pca.transform(X_train)
+    X_test_pca = pca.transform(X_test)
+
+
+    print("Fitting the classifier to the training set")
+    param_grid = {
+            'C': [1e3, 5e3, 1e4, 5e4, 1e5],
+            'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1],
+            }
+    clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
+    clf = clf.fit(X_train_pca, y_train)
+    print("Best estimator found by grid search:")
+    print(clf.best_estimator_)
+    
+    print("Predicting the people names on the testing set")
+    y_pred = clf.predict(X_test_pca)
+    
+    print(confusion_matrix(y_test, y_pred, labels=range(n_classes)))
+
+    prediction_titles = [title(y_pred, y_test, target_names, i) for i in range(y_pred.shape[0])]
+    plot_gallery(X_test, prediction_titles, h, w)
+
+    eigenface_titles = [f"eigenface {i}" for i in range(eigenfaces.shape[0])]
+    plot_gallery(eigenfaces, eigenface_titles, h, w)
+    plt.show()
+
+    from sklearn.metrics import accuracy_score
+    score = accuracy_score(y_test, y_pred)
+    print(score)
+    
+    return
